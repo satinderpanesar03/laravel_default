1e6bc28215e2bb8e214ad4bef18440975d56177e
satinder <satinderpanesar03@gmail.com>
2023-03-11 19:57:36 +0530

    initial

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LeadController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProspectController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'login'])->name('new_login');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+// ___________________________ Admin Route ____________________________
+Route::group(['middleware' => ['auth','is_Admin']], function () {
+    // manage-employees
+    Route::get('manage-employees', [UserController::class, 'index']);
+    Route::get('/delete-employee/{id}', [UserController::class, 'delete_employee']);
+    Route::get('/add-employee', [UserController::class, 'add_employee']);
+    Route::get('/edit-employee/{id}', [UserController::class, 'edit_employee']);
+    Route::put('/insert-employee', [UserController::class, 'insert_employee']);
+});
+
+
+// ___________________________ Admin & User Route ____________________________
+Route::group(['middleware' => ['auth','is_User']], function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    
+
+});
